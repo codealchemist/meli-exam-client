7ff0354decc4101f6144f06fc5c26940cc7bba9a
Alberto Miranda <codealchemist@gmail.com>
2019-01-13 20:50:23 -0300

    New build.

---
+++ build/precache-manifest.a7f618883e149df02a2ee6d6781c1883.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "24674c2e482d85a9e22b581d248a33da",
+    "url": "/static/media/img4.24674c2e.jpg"
+  },
+  {
+    "revision": "e1bd2e25924277f61066bf6fb55b6b60",
+    "url": "/static/media/img3.e1bd2e25.jpg"
+  },
+  {
+    "revision": "83b704293ef0bc6ce0e2ef6bb5af60be",
+    "url": "/static/media/img2.83b70429.jpg"
+  },
+  {
+    "revision": "dfbb53190b84474437cc2c4a5783648b",
+    "url": "/static/media/img1.dfbb5319.jpg"
+  },
+  {
+    "revision": "1033f8ef2c1f273d96e72eebd0863dad",
+    "url": "/static/media/empty-box.1033f8ef.gif"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "de2218e2824975a83043",
+    "url": "/static/js/main.de2218e2.chunk.js"
+  },
+  {
+    "revision": "e04a83af577ca40152d0",
+    "url": "/static/js/1.e04a83af.chunk.js"
+  },
+  {
+    "revision": "de2218e2824975a83043",
+    "url": "/static/css/main.b4698dbb.chunk.css"
+  },
+  {
+    "revision": "0b4e16e23be3d60e250aa2a92a89c28c",
+    "url": "/index.html"
+  }
+];
